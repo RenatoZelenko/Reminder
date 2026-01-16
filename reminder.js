a213d539c3b9234507bf58b03bdffb02e153a8ea
Renato <renato.zelenko2003@gmail.com>
2026-01-16 16:10:07 +0100

    Replaced files

---
+++ reminder.js
@@ -0,0 +1,14 @@
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyC9y61dLJ_mcw3S3h6s1nOfkkFz53mpIRQ",
+    authDomain: "reminder-db-57529.firebaseapp.com",
+    projectId: "reminder-db-57529",
+    storageBucket: "reminder-db-57529.firebasestorage.app",
+    messagingSenderId: "287428931967",
+    appId: "1:287428931967:web:3f5c0127277a1b69002a4a"
+  };
+
+  const app = initializeApp(firebaseConfig);
+</script>
